de986fb702b95012697371dea6a35264190e8b41
Basic-king <goatbasic4@gmail.com>
2025-04-04 20:34:16 +0100

    Update Key.js

--- Key.js
+++ Key.js
@@ -1,7 +1,7 @@
 loopy() 
  function loopy() { 
  var sWord ="@LostXofs" 
- while (sWord != "Basic king") {// Pass
+ while (sWord != "Basidbjs") {// Pass
  sWord = prompt("Enter The Uesr ")  
  } 
  
