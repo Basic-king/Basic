85c78d0daef516e0cd59042153afa683a7e09414
Basic-king <goatbasic4@gmail.com>
2025-04-04 20:38:50 +0100

    Create Basic.js

---
+++ Basic.js
@@ -0,0 +1,12 @@
+loopy() 
+ function loopy() { 
+ var sWord ="@LostXofs" 
+ while (sWord != "Basic king") {// Pass
+ sWord = prompt("Enter The Uesr ")  
+ } 
+ 
+//confirm("ok");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋  ");
+
+}
