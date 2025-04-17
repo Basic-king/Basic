c4d0d79045dcbbad43873bf88da622243533c04c
Basic-king <goatbasic4@gmail.com>
2025-04-17 16:48:55 +0100

    Create Wlh.js

---
+++ Wlh.js
@@ -0,0 +1,13 @@
+loopy() 
+ function loopy() { 
+ var sWord ="@BasicXofs" 
+ while (sWord != "BASIC IOS") {// Pass
+ sWord = prompt("Enter The Uesr ")  
+ } 
+ 
+//confirm("ok");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋  ");
+
+}
+
