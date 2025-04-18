65e1030d1da9ce3e39961cadb0afba18c46fc833
Basic-king <goatbasic4@gmail.com>
2025-04-18 16:14:07 +0100

    Create Vne.js

---
+++ Vne.js
@@ -0,0 +1,12 @@
+loopy() 
+ function loopy() { 
+ var sWord ="@BasicXofs" 
+ while (sWord != "Vanbao") {// Pass
+ sWord = prompt("Enter The Uesr ")  
+ } 
+ 
+//confirm("ok");
+window.alert("  BASIC KING ");
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋  ");
+
+}
