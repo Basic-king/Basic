92370aa4e4a87e0f5db727dc531f724efdb71fc4
Basic-king <goatbasic4@gmail.com>
2025-04-18 19:33:51 +0100

    Create Ereen.js

---
+++ Ereen.js
@@ -0,0 +1,12 @@
+loopy() 
+ function loopy() { 
+ var sWord ="@BasicXofs" 
+ while (sWord != "Basic king") {// Pass
+ sWord = prompt("Enter The Uesr ")  
+ } 
+ 
+//confirm("ok");
+window.alert("  BASIC KING  ");
+window.alert("  Welcome baby  ");
+
+}
